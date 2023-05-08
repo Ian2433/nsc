40dda6c9b449cbe940566526d32df963053ae2b2
Ian2433 <132889959+Ian2433@users.noreply.github.com>
2023-05-08 17:12:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var selectedRow = null
+
+function onFormSubmit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
+
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["firstName"] = document.getElementById("firstName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["enterAge"] = document.getElementById("enterAge").value;
+    formData["enterEmail"] = document.getElementById("enterEmail").value;
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.firstName;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.lastName;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.enterAge;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.enterEmail;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("firstName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("lastName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("enterAge").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("enterEmail").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.firstName;
+    selectedRow.cells[1].innerHTML = formData.lastName;
+    selectedRow.cells[2].innerHTML = formData.enterAge;
+    selectedRow.cells[3].innerHTML = formData.enterEmail;
+}
+
+//Delete the data
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+//Reset the data
+function resetForm() {
+    document.getElementById("firstName").value = '';
+    document.getElementById("lastName").value = '';
+    document.getElementById("enterAge").value = '';
+    document.getElementById("enterEmail").value = '';
+    selectedRow = null;
+}
